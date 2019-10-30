61d6817d5f4e89a3ec7f36193c37b02be4bb8535
jmako@looppay.com <jmako@jmako-mbp3.veracode.local>
2019-10-29 20:16:50 -0400

    New password to force scan

--- src/main/java/flawedPackage/Flawed.java
+++ src/main/java/flawedPackage/Flawed.java
@@ -12,7 +12,7 @@ public class Flawed {
 
 // some change
 
-	public static final String PASSWORD = "Rajeev";
+	public static final String PASSWORD = "Rajeevs";
 	static byte[] dec;
 
 	public static void main(String[] args) throws Exception {
